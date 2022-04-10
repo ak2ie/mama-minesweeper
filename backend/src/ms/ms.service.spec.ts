0625fdbe3e9a6ee96cd946b27145ec8876df3b10
ak2ie <mail@ak2ie.net>
2022-04-10 14:52:05 +0900

    テスト実施可能

--- backend/src/ms/ms.service.spec.ts
+++ backend/src/ms/ms.service.spec.ts
@@ -1,14 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CreateMSDto } from './dto/create-ms.dto';
 import { MsService } from './ms.service';
-import { getRepository, Collection } from 'fireorm';
+import { MineSweeper } from './models/ms.model';
 
-let getRepositoryMMock;
 jest.mock('fireorm', () => {
-  const CollectionMMock = Collection as jest.Mock;
-  CollectionMMock.mockReturnThis();
-  getRepositoryMMock = getRepository as jest.Mock;
-});
+    return {
+        Collection: () => jest.fn(),
+        getRepository: () => ({
+            create: (_) => {
+              const ms =  new MineSweeper()
+              ms.id = "1";
+              return ms;
+            },
+        })
+    }
+})
 
 describe('MsService', () => {
   let service: MsService;
@@ -19,20 +25,31 @@ describe('MsService', () => {
     }).compile();
 
     service = module.get<MsService>(MsService);
+    jest.resetModules();
+    jest.doMock("fireorm");
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  it('invalid call', async () => {
+  xit('URLが空', async () => {
     const dto = new CreateMSDto();
     dto.panels = [{ imageUrl: '', isBomb: false }];
-    getRepositoryMMock.mockImplementation((aa) => {
-      return {
-        create: () => {},
-      };
-    });
-    expect(await service.create(dto)).toBe('');
+    await expect(service.create(dto)).rejects.toThrow();
+  });
+
+  xit('地雷なし', async () => {
+    const dto = new CreateMSDto();
+    dto.panels = [{ imageUrl: 'dummy', isBomb: false }, { imageUrl: 'dummy', isBomb: false }];
+    await expect(service.create(dto)).rejects.toThrow();
+  });
+
+  xit('すべて地雷', async () => {
+    const dto = new CreateMSDto();
+    dto.panels = [{ imageUrl: 'dummy', isBomb: true }, { imageUrl: 'dummy', isBomb: true }];
+    await expect(service.create(dto)).rejects.toThrow();
+  });
+
+  it('正常', async () => {
+    const dto = new CreateMSDto();
+    dto.panels = [{ imageUrl: 'dummy1', isBomb: true }, { imageUrl: 'dummy2', isBomb: false }];
+    expect(await service.create(dto)).toBe("1");
   });
 });
--- backend/src/ms/ms.service.ts
+++ backend/src/ms/ms.service.ts
@@ -7,12 +7,24 @@ import { MineSweeper } from './models/ms.model';
 @Injectable()
 export class MsService {
   async create(createMSDto: CreateMSDto) {
+    const isAllUrlSet = createMSDto.panels.every((panel) => panel.imageUrl !== "");
+    if (!isAllUrlSet) {
+      throw new Error("URLが設定されていないマスがあります。")
+    }
+    const existsBomb = createMSDto.panels.some(panel => panel.isBomb)
+    if (!existsBomb) {
+      throw new Error("地雷が設定されていません")
+    }
+    const isAllBomb = createMSDto.panels.every(panel => panel.isBomb)
+    if (isAllBomb){
+      throw new Error("すべてのマスが地雷です")
+    }
     const msRepository = getRepository(MineSweeper);
     const ms = new MineSweeper();
     ms.panels = [{ imageUrl: '', isBomb: false }];
     ms.createdAt = new Date();
-    await msRepository.create(ms);
-    return 'This action adds a new m';
+    const newDoc = await msRepository.create(ms);
+    return newDoc.id;
   }
 
   findAll() {
