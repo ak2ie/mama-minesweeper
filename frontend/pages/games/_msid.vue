0cff9eae938f878e57513de709e2bf9df2dd7e9a
ak2ie <mail@ak2ie.net>
2022-04-12 23:37:32 +0900

    API呼び出しページを作成

---
+++ frontend/pages/games/_msid.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6"> id: {{ msid }} </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import axios from 'axios'
+
+export type DataType = {
+  msid: string
+}
+
+export default Vue.extend({
+  data(): DataType {
+    return {
+      msid: '',
+    }
+  },
+  head() {
+    return {
+      title: 'マインスイーパ',
+    }
+  },
+  mounted() {},
+  created() {
+    this.msid = this.$route.path.replace('/games/', '')
+    if (this.msid.match(/[0-9a-zA-Z]+/)) {
+      axios.get(
+        `https://asia-northeast1-mama-ms.cloudfunctions.net/api/ms/${this.msid}`
+      )
+    }
+  },
+})
+</script>
