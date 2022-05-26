abb708bd8ae236f65b762f4810eb563b51bf8382
Kazuki Imamura <kaizumaki@gmail.com>
2022-05-26 09:48:10 +0900

    proxyを再設定

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -49,7 +49,11 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    proxy: true,
+  },
+
+  proxy: {
+    '/services/rest/': 'https://api1-ap.webpurify.com',
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
